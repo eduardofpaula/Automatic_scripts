c7c2fc68bc18eb95d01286677cb72a1e5e3dc43d
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2025-09-06 02:07:42 -0300

    fix: corrigindo a ordem de instalação

--- oracle_database/oracle-EE/21.3.0/init-scripts/04_install_schemas.sql
+++ oracle_database/oracle-EE/21.3.0/init-scripts/04_install_schemas.sql
@@ -12,21 +12,21 @@ CONNECT sys/EduLindo123@ORCLPDB1 AS SYSDBA;
 COMMIT;
 
 CONNECT sys/EduLindo123@ORCLPDB1 AS SYSDBA;
-@/opt/oracle/sample-schemas/product_media/pm_main.sql pm pm pm_temp oe EduLindo123 /opt/oracle/sample-schemas/product_media/ /opt/oracle/sample-schemas/log/ /opt/oracle/sample-schemas/product_media/ "localhost:1521/ORCLPDB1"
-
-COMMIT;
-
-CONNECT sys/EduLindo123@ORCLPDB1 AS SYSDBA;
-@/opt/oracle/sample-schemas/info_exchange/ix_main.sql ix ix ix_temp EduLindo123 /opt/oracle/sample-schemas/log/ v3 "localhost:1521/ORCLPDB1"
-
-COMMIT;
-
-CONNECT sys/EduLindo123@ORCLPDB1 AS SYSDBA;
 @/opt/oracle/sample-schemas/sales_history/sh_main.sql sh sh sh_temp EduLindo123 /opt/oracle/sample-schemas/sales_history/ /opt/oracle/sample-schemas/log/ v3 "localhost:1521/ORCLPDB1" 
 
 COMMIT;
 
 CONNECT sys/EduLindo123@ORCLPDB1 AS SYSDBA;
+@/opt/oracle/sample-schemas/product_media/pm_main.sql pm pm pm_temp oe EduLindo123 /opt/oracle/sample-schemas/product_media/ /opt/oracle/sample-schemas/log/ /opt/oracle/sample-schemas/product_media/ "localhost:1521/ORCLPDB1"
+
+COMMIT;
+
+CONNECT sys/EduLindo123@ORCLPDB1 AS SYSDBA;
 @/opt/oracle/sample-schemas/bus_intelligence/bi_main.sql bi bi bi_temp EduLindo123 oe sh /opt/oracle/sample-schemas/log/ v3 "localhost:1521/ORCLPDB1"
+
+COMMIT;
+
+CONNECT sys/EduLindo123@ORCLPDB1 AS SYSDBA;
+@/opt/oracle/sample-schemas/info_exchange/ix_main.sql ix ix ix_temp EduLindo123 /opt/oracle/sample-schemas/log/ v3 "localhost:1521/ORCLPDB1"
 
 COMMIT;

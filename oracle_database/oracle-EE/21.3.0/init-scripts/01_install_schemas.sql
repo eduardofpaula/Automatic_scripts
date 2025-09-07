0e70bdecf594a8461f637cdca87df45dd8d765d4
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2025-09-07 19:04:59 -0300

    feat: define variables for schema installation and update connection method in initialization script

--- oracle_database/oracle-EE/21.3.0/init-scripts/01_install_schemas.sql
+++ oracle_database/oracle-EE/21.3.0/init-scripts/01_install_schemas.sql
@@ -1,12 +1,21 @@
--- filepath: /home/eduardo/Documents/Repos/Automatic_scripts/oracle_database/oracle-EE/21.3.0/init-scripts/04_install_schemas.sql
 ALTER SESSION SET CONTAINER = ORCLPDB1;
 
--- Conectar como SYSTEM para executar o script principal da Oracle
-CONNECT system/EduLindo123@localhost:1521/ORCLPDB1;
+-- Defina as variáveis
+DEFINE SYSTEM_password = EduLindo123
+DEFINE SYS_password    = EduLindo123
+DEFINE HR_password     = hr
+DEFINE OE_password     = oe
+DEFINE PM_password     = pm
+DEFINE IX_password     = ix
+DEFINE SH_password     = sh
+DEFINE BI_password     = bi
+DEFINE default_ts      = USERS
+DEFINE temp_ts         = TEMP
+DEFINE log_dir         = /opt/oracle/sample-schemas/log
+DEFINE connect_string  = localhost:1521/ORCLPDB1
 
--- Executar o script oficial da Oracle conforme README.txt seção 2.4
--- @mksample <SYSTEM_password> <SYS_password> <HR_password> <OE_password> <PM_password> <IX_password> <SH_password> <BI_password> <default_tablespace> <temp_tablespace> <log_directory> <connect_string>
+-- Conecta e cria diretórios
+CONNECT system/&SYSTEM_password@&connect_string
 
-@/opt/oracle/sample-schemas/mksample.sql EduLindo123 EduLindo123 hr oe pm ix sh bi USERS TEMP /opt/oracle/sample-schemas/log/ localhost:1521/ORCLPDB1
-
-COMMIT;
+-- Executa o mksample
+@/opt/oracle/sample-schemas/mksample &SYSTEM_password &SYS_password &HR_password &OE_password &PM_password &IX_password &SH_password &BI_password &default_ts &temp_ts &log_dir &connect_string
